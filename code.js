a88b1d4a4e198c90ef14fa7c072af702515cf5bb
JacobMorgan3 <144738756+JacobMorgan3@users.noreply.github.com>
2024-03-06 17:34:16 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,31 @@
 function depthFirstSearch(graph, startNode, targetNode) {
+    
+    let stack = [];
+    let path = [];
+    stack.push(startNode);
+    path.push(startNode);
+    
+    while (stack.length > 0) {
+        if (stack.length == targetNode) {
+            return path;
+        }
+        for (let i = 0; i < stack[stack.length].length; i++) { //push adjacent vertices to the stack
+            stack.push(i)
+        }
+        stack.pop(stack.length); //remove the vertix we are currently at
+        
+        
+        
+    }
+    
     return [];
 }
+
+
+startNode = 0;
+
+targetNode = 2;
+
+graph = [[1,2], [0,2], [0,1]];
+
+console.log(depthFirstSearch(graph, startNode, targetNode));
