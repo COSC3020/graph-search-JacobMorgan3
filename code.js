a66dc1f14ee64a9c69838cdd736623bf5855e610
JacobMorgan3 <144738756+JacobMorgan3@users.noreply.github.com>
2024-03-20 16:47:48 -0600

    Update code.js

--- code.js
+++ code.js
@@ -4,28 +4,53 @@ function depthFirstSearch(graph, startNode, targetNode) {
     let path = [];
     stack.push(startNode);
     path.push(startNode);
+    let i = 0;
     
-    while (stack.length > 0) {
-        if (stack.length == targetNode) {
+    while (true) {
+        
+        if (stack[stack.length - 1] == targetNode) {
             return path;
         }
-        for (let i = 0; i < stack[stack.length].length; i++) { //push adjacent vertices to the stack
-            stack.push(i)
+        
+        tmp = stack[stack.length - 1];
+        stack.pop();
+        
+        for (let i = 0; i < graph[tmp].length; i++) { //push adjacent vertices to the stack
+            stack.push(graph[tmp][i]);
+            path.push(graph[tmp][i]);
+            
+            while (true) {
+                if (stack[stack.length - 1] == targetNode) {
+                    return path;
+                }
+                else {
+                    stack.pop();
+                    path.pop();
+                }
+                if (stack.length == 0) {
+                    break;
+                }
+            }
         }
-        stack.pop(stack.length); //remove the vertix we are currently at
+    
+        if (graph.length == i) {
+            return "Node not found."
+        }
         
-        
-        
+        i++;
+        stack.push(graph[startNode + i]);
+        path.push(i);
+        path.push(graph[i][graph[i].length - 1])
     }
     
-    return [];
+    return;
 }
 
 
 startNode = 0;
 
-targetNode = 2;
+targetNode = 3;
 
-graph = [[1,2], [0,2], [0,1]];
+graph = [[1,4],[3],[0],[2],[1,3]];
 
 console.log(depthFirstSearch(graph, startNode, targetNode));
