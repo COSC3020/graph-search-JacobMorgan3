ed1416c226546c0a9f998b4d1751bf6dd5836cc4
JacobMorgan3 <144738756+JacobMorgan3@users.noreply.github.com>
2024-03-28 16:15:02 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,10 +1,5 @@
-const fs = require('fs');
-const jsc = require('jsverify');
+const code = require(" ./code.js");
+const assert = require("assert");
 
-eval(fs.readFileSync('code.js')+'');
-
-const test =
-    jsc.forall("array nat", function(arr) {
-        return JSON.stringify(depthFirstSearch(arr, startNode, targetNode)) == [ 0, 1, 4, 3, 2 ];
-    });
-jsc.assert(test);
+//test if startNode == targetNode then return []
+assert(depthFirstSearch(graph, startNode, targetNode) == []);
