28cecefb28c8d5e38a74bd352bbb4673d3ee28dd
JacobMorgan3 <144738756+JacobMorgan3@users.noreply.github.com>
2024-03-28 16:30:54 -0600

    testing code.test.js

--- code.test.js
+++ code.test.js
@@ -1,5 +1,12 @@
 const code = require(" ./code.js");
 const assert = require("assert");
 
+//assign values for graph, startNode, targetNode
+let graph = [[1,4],[3],[0],[2],[1,3]];
+let startNode = 0;
+let targetNode = 0;
+
+
+
 //test if startNode == targetNode then return []
-assert(depthFirstSearch(graph, startNode, targetNode) == []);
+assert(code.depthFirstSearch(graph, startNode, targetNode) == [0]);
