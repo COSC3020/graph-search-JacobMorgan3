82cae83a8d9e051c13755b5a38df0c3401ea2c59
JacobMorgan3 <144738756+JacobMorgan3@users.noreply.github.com>
2024-03-28 16:43:59 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,12 +1,18 @@
 const code = require("./code.js");
 const assert = require("assert");
 
+//function for arrayEqualTo
+function arrayEqual(a, b) {
+  return JSON.stringify(a) == JSON.stringify(b); 
+}
+
 //assign values for graph, startNode, targetNode
 let graph = [[1,4],[3],[0],[2],[1,3]];
 let startNode = 0;
 let targetNode = 0;
 
-
+let a = code.depthFirstSearch(graph, startNode, targetNode);
+let b = [0];
 
 //test if startNode == targetNode then return []
-assert(code.depthFirstSearch(graph, startNode, targetNode) == [0]);
+assert(arrayEqual(a,b);
