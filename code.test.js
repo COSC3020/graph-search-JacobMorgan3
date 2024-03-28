83be3fbb1865437b001deef151c7631a5bf17a68
JacobMorgan3 <144738756+JacobMorgan3@users.noreply.github.com>
2024-03-28 16:05:11 -0600

    Trying to figure out jsc.assert   code.test.js

--- code.test.js
+++ code.test.js
@@ -1,7 +1,10 @@
+const fs = require('fs');
+const jsc = require('jsverify');
 
-describe('`sdfs`', () => {
-    // unit test:
-    it('dfs `[[1,4],[3],[0],[2],[1,3]]` ', () => {
-        expect(dfs([[1,4],[3],[0],[2],[1,3]]), 0, 2).to.deep.equal([ 0, 1, 4, 3, 2 ])
-    })
-})
+eval(fs.readFileSync('code.js')+'');
+
+const test =
+    jsc.forall("array nat", function(arr) {
+        return JSON.stringify(depthFirstSearch(arr, startNode, targetNode)) == [ 0, 1, 4, 3, 2 ];
+    });
+jsc.assert(test);
