869017b861d3eeeba1953a75ec23812840d4bd2f
JacobMorgan3 <144738756+JacobMorgan3@users.noreply.github.com>
2024-03-27 18:48:58 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,55 +1,42 @@
 function depthFirstSearch(graph, startNode, targetNode) {
     
     let stack = [];
-    let path = [];
+    let visited = [];
+    let isThere = false;
+    
     stack.push(startNode);
-    path.push(startNode);
-    let i = 0;
     
     while (true) {
-        
+        visited.push(stack[stack.length - 1]);
         if (stack[stack.length - 1] == targetNode) {
-            return path;
+        return visited;
         }
-        
-        tmp = stack[stack.length - 1];
         stack.pop();
         
-        for (let i = 0; i < graph[tmp].length; i++) { //push adjacent vertices to the stack
-            stack.push(graph[tmp][i]);
-            path.push(graph[tmp][i]);
-            
-            while (true) {
-                if (stack[stack.length - 1] == targetNode) {
-                    return path;
-                }
-                else {
-                    stack.pop();
-                    path.pop();
-                }
-                if (stack.length == 0) {
-                    break;
+        for (let i = 0; i < graph[visited[visited.length - 1]].length; i++) { //put adjacent nodes on the stack
+            for (let j = 0; j < visited.length; j++) {
+                if (visited[j] == graph[visited[visited.length - 1]][i]) {
+                    isThere = true;
                 }
             }
+            if (isThere == false) { // if not already in visited push it to the stack
+                stack.push(graph[visited[visited.length - 1]][i]);
+            }
         }
-    
-        if (graph.length == i) {
-            return "Node not found."
-        }
+        isThere = false; //set isThere back to false for the next loop 
         
-        i++;
-        stack.push(graph[startNode + i]);
-        path.push(i);
-        path.push(graph[i][graph[i].length - 1])
+        
+        //console.log("new loop")
+        //console.log("visited: " + visited);
+        //console.log("stack: " + stack);   
     }
-    
-    return;
 }
+    
 
 
 startNode = 0;
 
-targetNode = 3;
+targetNode = 2;
 
 graph = [[1,4],[3],[0],[2],[1,3]];
 
