51ace6c7463e2bed3b3d0cd5741f2d06ba1f9d09
JacobMorgan3 <144738756+JacobMorgan3@users.noreply.github.com>
2024-03-27 21:44:54 -0600

    Update code.js

--- code.js
+++ code.js
@@ -6,7 +6,7 @@ function depthFirstSearch(graph, startNode, targetNode) {
     stack.push(startNode);
     
     while (stack.length > 0) {
-        console.log("new loop");
+        //console.log("new loop");
         visited.push(stack[stack.length - 1]);
         if (stack[stack.length - 1] == targetNode) {
             return visited;
@@ -22,11 +22,11 @@ function depthFirstSearch(graph, startNode, targetNode) {
             } 
             if (isThere == false) { // if not already in visited push it to the stack
                 stack.push(graph[tmp][i]);
-                console.log("this is added to the stack: " + graph[tmp][i]);
+                //console.log("this is added to the stack: " + graph[tmp][i]);
             }
         }
-        console.log("visited: " + visited);
-        console.log("stack: " + stack);
+        //console.log("visited: " + visited);
+        //console.log("stack: " + stack);
     }
     
     return [];
@@ -40,4 +40,32 @@ targetNode = 1;
 
 graph = [[1,4],[3],[0],[2],[1,3]];
 
-console.log(depthFirstSearch(graph, startNode, targetNode));
+console.log(breadthFirstSearch(graph, startNode, targetNode));
+
+
+function breadthFirstSearch (graph, startNode, targetNode) {
+    let isThere = false;
+    let visited = [];
+    let queue = [];
+    queue.push(startNode);
+    
+    while (queue.length > 0) {
+        
+        if (queue[0] == targetNode) {
+            return visited;
+        }
+        visited.push(queue[0]);
+        let tmp = queue.shift;
+        
+        for (let i = 0; i < graph[tmp].length; i++) {
+            for (let j = 0; j < visited.length; j++) {
+                if (visited[j] == graph[tmp][i]) {
+                    isThere = true;
+                }
+            } 
+            if (isThere == false) { // if not already in visited push it to the stack
+                queue.push(graph[tmp][i]);
+                //console.log("this is added to the queue: " + graph[tmp][i]);
+            }
+    }
+}
