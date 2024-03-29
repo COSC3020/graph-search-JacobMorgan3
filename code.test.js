5fa27c81332b53754953f5abb638924ea84dcc38
JacobMorgan3 <144738756+JacobMorgan3@users.noreply.github.com>
2024-03-29 11:21:25 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -6,6 +6,21 @@ function arrayEqual(a, b) {
   return JSON.stringify(a) == JSON.stringify(b); 
 }
 
+//function for noRepeated nodes in answer path
+function noRepeats (a, b) {
+  let count = 0;
+  for (let i = 0; i < a.length; i++) {
+    for (let j = 0; j < a.length; j++) {
+      if (a[i] == a[j]) {
+        count++;
+      }
+    }
+  }
+  return count == b;
+}
+
+
+///////////////////////////////////////////////// DFS Test 1: if startNode is the targetNode, then return startNode /////////////////////////////////
 //assign values for graph, startNode, targetNode
 let graph = [[1,4],[3],[0],[2],[1,3]];
 let startNode = 0;
@@ -14,5 +29,33 @@ let targetNode = 0;
 let a = code.depthFirstSearch(graph, startNode, targetNode);
 let b = [0];
 
-//test if startNode == targetNode then return []
+//test 
 assert(arrayEqual(a,b));
+
+
+///////////////////////////////////////////////// DFS Test 2: if targetNode is not there then return [] /////////////////////////////////
+//assign values for graph, startNode, targetNode
+let graph = [[1,4],[3],[0],[2],[1,3]];
+let startNode = 0;
+let targetNode = 7;
+
+let a = code.depthFirstSearch(graph, startNode, targetNode);
+let b = 0;
+
+//test 
+assert(arrayEqual(a,b));
+
+
+///////////////////////////////////////////////// DFS Test 3: The anser should not have repeated nodes /////////////////////////////////
+//assign values for graph, startNode, targetNode
+let graph = [[1,4],[3],[0],[2],[1,3]];
+let startNode = 0;
+let targetNode = 2;
+
+let a = code.depthFirstSearch(graph, startNode, targetNode);
+let b = 4;
+
+//test 
+assert(noRepeats(a,b));
+
+
