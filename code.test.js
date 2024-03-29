b1ee691fcf7c671f978f84648c85d46b5817af5e
JacobMorgan3 <144738756+JacobMorgan3@users.noreply.github.com>
2024-03-29 11:28:41 -0600

    Added testing for BFS code.test.js

--- code.test.js
+++ code.test.js
@@ -46,7 +46,7 @@ b = [];
 assert(arrayEqual(a,b));
 
 
-///////////////////////////////////////////////// DFS Test 3: The anser should not have repeated nodes /////////////////////////////////
+///////////////////////////////////////////////// DFS Test 3: The answer should not have repeated nodes /////////////////////////////////
 //assign values for graph, startNode, targetNode
 graph = [[1,4],[3],[0],[2],[1,3]];
 startNode = 0;
@@ -59,3 +59,43 @@ b = 4;
 assert(noRepeats(a,b));
 
 
+
+
+
+///////////////////////////////////////////////// BFS Test 1: if startNode is the targetNode, then return startNode /////////////////////////////////
+//assign values for graph, startNode, targetNode
+graph = [[1,4],[3],[0],[2],[1,3]];
+startNode = 0;
+targetNode = 0;
+
+a = code.breadthFirstSearch(graph, startNode, targetNode);
+b = [0];
+
+//test 
+assert(arrayEqual(a,b));
+
+
+///////////////////////////////////////////////// BFS Test 2: if targetNode is not there then return [] /////////////////////////////////
+//assign values for graph, startNode, targetNode
+graph = [[1,4],[3],[0],[2],[1,3]];
+startNode = 0;
+targetNode = 7;
+
+a = code.breadthFirstSearch(graph, startNode, targetNode);
+b = [];
+
+//test 
+assert(arrayEqual(a,b));
+
+
+///////////////////////////////////////////////// BFS Test 3: The answer should not have repeated nodes /////////////////////////////////
+//assign values for graph, startNode, targetNode
+graph = [[1,4],[3],[0],[2],[1,3]];
+startNode = 0;
+targetNode = 2;
+
+a = code.breadthFirstSearch(graph, startNode, targetNode);
+b = 4;
+
+//test 
+assert(noRepeats(a,b));
